040adfbca67b3cf04eacb9d1133173d3d103e96c
Zihan Wang <zihan9701@gmail.com>
2025-07-07 22:33:57 -0700

    Refactor project structure: add FastAPI app, Dockerfile, requirements, and update .gitignore; remove old backend files

---
+++ app/src/constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+ROOT = Path(__file__).parent.parent
+
+ASSERTS_PATH = ROOT / "static" / "interview_questions" / "assets"
+
+INTERVIEW_QUESTIONS_PATH = ROOT / "static" / "interview_questions" / "InterviewQuestionList.json"
+ENTRY_POINT_INTERVIEW_QUESTIONS_PATH = ROOT / "static" / "interview_questions" / "index.html"
---
+++ app/src/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from pathlib import Path
+from .constants import (INTERVIEW_QUESTIONS_PATH, 
+                           ENTRY_POINT_INTERVIEW_QUESTIONS_PATH,
+                           ASSERTS_PATH)
+
+
+INTERVIEW_QUESTIONS = "/InterviewQuestionList.json"
+
+app = FastAPI()
+
+app.mount(
+    "/assets",
+    StaticFiles(directory=ASSERTS_PATH),
+    name="assets"
+)
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    index_path = Path(__file__).parent / "static" / "index.html"
+    return index_path.read_text(encoding="utf-8")
+
+@app.get(INTERVIEW_QUESTIONS)
+async def serve_question_list():
+    json_path = INTERVIEW_QUESTIONS_PATH
+    return FileResponse(json_path, media_type="application/json")
+
+@app.get("/interview_questions", response_class=HTMLResponse)
+async def read_interview_questions():
+    index_path = ENTRY_POINT_INTERVIEW_QUESTIONS_PATH
+    return index_path.read_text(encoding="utf-8")
