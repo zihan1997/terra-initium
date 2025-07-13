a31ce1177e087df38d17dade29f71063db447c9b
Zihan Wang <zihan9701@gmail.com>
2025-07-13 09:51:55 -0700

    add UI part

--- app/src/constants.py
+++ app/src/constants.py
@@ -4,5 +4,5 @@ ROOT = Path(__file__).parent.parent
 
 ASSERTS_PATH = ROOT / "static" / "interview_questions" / "assets"
 
-INTERVIEW_QUESTIONS_PATH = ROOT / "static" / "interview_questions" / "InterviewQuestionList.json"
+INTERVIEW_QUESTIONS_PATH = ROOT / "static" / "InterviewQuestionList.json"
 ENTRY_POINT_INTERVIEW_QUESTIONS_PATH = ROOT / "static" / "interview_questions" / "index.html"
