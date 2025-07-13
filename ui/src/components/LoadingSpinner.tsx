67cf4e12aaeeff7ede03c74e75dd1ad1ef694f7f
zihan1997 <zihan9701@gmail.com>
2025-07-12 23:28:57 -0700

    Revert "[ui] add existing react"

--- ui/src/components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from 'lucide-react';
-
-export function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center p-8">
-      <Loader2 className="w-8 h-8 animate-spin text-indigo-600" />
-    </div>
-  );
-}
