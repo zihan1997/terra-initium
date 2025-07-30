2d027f99fed21e3eacc5119bdb2670bbd180dfd8
zihan1997 <zihan9701@gmail.com>
2025-07-29 22:40:20 -0700

    add simple log in

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react';
+import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import SimpleLogIn from './components/SimpleLogIn.tsx';
+
+function WebLogin(){
+  const [canPass, setCanPass] = useState(false);
+  return (
+    canPass? <App />
+      : <SimpleLogIn
+      onLogin={() => setCanPass(true)}
+      />
+  );
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WebLogin />
   </StrictMode>
 );
