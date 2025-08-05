2bb494062b4007470acbaac75e47472f07e10a43
zihan1997 <zihan9701@gmail.com>
2025-08-04 17:58:12 -0700

    add base64 encryption

--- app/src/interview_questions/router.py
+++ app/src/interview_questions/router.py
@@ -1,5 +1,10 @@
+import json
+import typing
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse, FileResponse
+from pydantic import BaseModel
+import base64
+
 from src.constants import ENTRY_POINT_INTERVIEW_QUESTIONS_PATH, INTERVIEW_QUESTIONS_PATH
 
 router = APIRouter(
@@ -8,10 +13,30 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@router.get('/InterviewQuestionList.json')
+class Question(BaseModel):
+    id: int
+    question: str
+    answer: str | None
+    keyword: str
+    frequency: int
+    top: bool
+
+@router.get('/interview-question-list')
 async def serve_question_list():
     json_path = INTERVIEW_QUESTIONS_PATH
-    return FileResponse(json_path, media_type="application/json")
+    with open(json_path, 'r') as f:
+        raw = json.load(f)
+    questions = [Question(**q) for q in raw]
+    # return questions
+    questions: typing.List[Question] = [Question(**q) for q in raw]
+    return [{
+        'id': q.id,
+        'keyword': q.keyword,
+        'question': base64.b64encode(q.question.encode()),
+        'answer': base64.b64encode(q.answer.encode())if  q.answer else '',
+        'frequency': q.frequency,
+        'top' : q.top
+    }for q in questions]
 
 @router.get("/", response_class=HTMLResponse)
 async def read_root():
--- ui/src/utils/loadQuestions.ts
+++ ui/src/utils/loadQuestions.ts
@@ -2,11 +2,18 @@ import type { Question } from '../types/Question';
 
 export async function loadQuestions(): Promise<Question[]> {
   try {
-    const response = await fetch('./InterviewQuestionList.json');
+    const response = await fetch('./interview-question-list');
     if (!response.ok) {
       throw new Error('Failed to load questions');
     }
-    return response.json();
+    const encryptedQuestions: Question[] = await response.json();
+    return encryptedQuestions.map(question => {
+      return {
+        ...question,
+        question: atob(question.question),
+        answer: question.answer? atob(question.answer): ''
+      }
+    })
   } catch (error) {
     console.error('Error loading questions:', error);
     return [];
